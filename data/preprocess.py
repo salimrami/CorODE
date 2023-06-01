9a8f25c790c93e8c1b2ae5a8f2c06e83e933cff1
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-01 16:58:50 +0200

    ges

--- data/preprocess.py
+++ data/preprocess.py
@@ -23,8 +23,8 @@ def process_surface(v, f, data_name='fetal'):
     f = f.astype(np.float32)
 
     if data_name == 'fetal':
-        #v = v[:,[2,1,0]].copy()
-        #f = f[:,[2,1,0]].copy()
+        v = v[:,[2,1,0]].copy()
+        f = f[:,[2,1,0]].copy()
         # normalize to [-1, 1]
         v = (v - [104, 104, 78]) / 104
     else:
@@ -34,8 +34,8 @@ def process_surface(v, f, data_name='fetal'):
 
 def process_surface_inverse(v, f, data_name='fetal'):
     if data_name == 'fetal':
-        #v = v * 104 + [104, 104, 78]
-        #v = v[:,[2,1,0]].copy()
+        v = v * 104 + [104, 104, 78]
+        v = v[:,[2,1,0]].copy()
         f = f[:,[2,1,0]].copy()
     else:
         raise ValueError("data_name should be in ['fetal']")
