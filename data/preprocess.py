d36fd6ad9d1927fb89965204f8bef7e6cf497c6c
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-01 16:57:56 +0200

    ges

--- data/preprocess.py
+++ data/preprocess.py
@@ -9,27 +9,22 @@ we recommend to set data_name='adni' with slight modification on the image clipp
 Note: it is tedious to tune the transformation of the surface to match the volume
 for a new dataset, but the matching is important to make everything work.
 """
-
-
+from eval import seg2surf
 import numpy as np
 
 def process_volume(x, data_name='fetal'):
-
-    
     if data_name == 'fetal':
         x = np.pad(x, ((2,2),(0,0),(0,0)), 'constant', constant_values=0)
         return x[None].copy()
     else:
         raise ValueError("data_name should be in ['fetal']")
 
-
 def process_surface(v, f, data_name='fetal'):
     f = f.astype(np.float32)
-    
-    
+
     if data_name == 'fetal':
-        v = v[:,[2,1,0]].copy()
-        f = f[:,[2,1,0]].copy()
+        #v = v[:,[2,1,0]].copy()
+        #f = f[:,[2,1,0]].copy()
         # normalize to [-1, 1]
         v = (v - [104, 104, 78]) / 104
     else:
@@ -37,22 +32,16 @@ def process_surface(v, f, data_name='fetal'):
 
     return v, f
 
-
 def process_surface_inverse(v, f, data_name='fetal'):
-    """
-    inversed preprocessing to transform the surface to its original space
-    """
-    
-    
-        
     if data_name == 'fetal':
-        v = v * 104 + [104, 104, 78]
-        v = v[:,[2,1,0]].copy()
-        f = f[:,[2,1,0]].copy()        
+        #v = v * 104 + [104, 104, 78]
+        #v = v[:,[2,1,0]].copy()
+        f = f[:,[2,1,0]].copy()
     else:
         raise ValueError("data_name should be in ['fetal']")
 
     return v, f
+
 def process_segmentation(seg, data_name='fetal'):
     if data_name == 'fetal':
         seg = np.pad(seg, ((2, 2), (0, 0), (0, 0)), 'constant', constant_values=0)
