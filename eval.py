a35f66e201207cc1e64a6f43d912823e1deed427
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-02 13:09:02 +0200

    seg

--- eval.py
+++ eval.py
@@ -41,7 +41,9 @@ def seg2surf(seg,
     """
     
     # ------ connected components checking ------ 
-    cc, nc = compute_cc(seg, connectivity=2, return_num=True)
+    #cc, nc = compute_cc(seg, connectivity=2, return_num=True)
+    cc, nc = compute_cc(seg[0], connectivity=2, return_num=True)
+
     cc_id = 1 + np.argmax(np.array([np.count_nonzero(cc == i)\
                                     for i in range(1, nc+1)]))
     seg = (cc==cc_id).astype(np.float64)
