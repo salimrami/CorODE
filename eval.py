143ea190cf2ce92f758a30563ecaf609e5d95a5c
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-02 13:54:30 +0200

    seg

--- eval.py
+++ eval.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
 
     # ------ load models ------
     
-    seg_file = "/scratch/saiterrami/seg/seg_img.nii.gz"
+    seg_file = "/Users/salimrami/Desktop/exp/lh.nii.gz"
     print(seg_file)
 
 # Load the segmentation data
