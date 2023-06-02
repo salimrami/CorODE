98f74611051b78c56b125b7b763a408555476f51
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-02 13:06:41 +0200

    seg

--- data/preprocess.py
+++ data/preprocess.py
@@ -9,7 +9,7 @@ we recommend to set data_name='adni' with slight modification on the image clipp
 Note: it is tedious to tune the transformation of the surface to match the volume
 for a new dataset, but the matching is important to make everything work.
 """
-from eval import seg2surf
+#from eval import seg2surf
 import numpy as np
 
 def process_volume(x, data_name='fetal'):
