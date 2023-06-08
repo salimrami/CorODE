f73ab3beeb3637e6e153d9266edb9f0cea7ca99b
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-08 16:23:53 +0200

    l

--- eval.py
+++ eval.py
@@ -113,7 +113,7 @@ if __name__ == '__main__':
 
 # Load the segmentation data
     seg_data = nib.load(seg_file).get_fdata()
-    seg_data = np.pad(seg_data, ((2,2),(0,0),(0,0)), 'constant', constant_values=0)
+    #seg_data = np.pad(seg_data, ((2,2),(0,0),(0,0)), 'constant', constant_values=0)
     
     #seg_data =process_volume(seg_data)
     #seg_data = process_volume(seg_data, data_name='fetal')
