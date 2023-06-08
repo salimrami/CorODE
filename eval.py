e5bf45f4f13c07459336dcf8580f6e6ebc5ffb3b
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-08 22:37:27 +0200

    l

--- eval.py
+++ eval.py
@@ -125,11 +125,12 @@ if __name__ == '__main__':
     """
     # Load the segmentation data
     seg_data = nib.load(seg_file).get_fdata()
-    seg_data = torch.from_numpy(seg_data)  # Convert to a PyTorch tensor
     seg_pred = seg_data.permute(1, 2, 0)  # Adjust the dimensions for the process_volume function
     seg_pred = process_volume(seg_pred, data_name='fetal')  # Preprocess seg_pred
     seg_pred = seg_pred.squeeze(0)  # Remove the batch dimension
     seg_pred = seg_pred.permute(2, 0, 1)  # Adjust the dimensions back
+    seg_data = torch.from_numpy(seg_data)  # Convert to a PyTorch tensor
+    
 
     seg_data = seg_data.to(device)  # Move seg_data to GPU (if necessary)
     # seg_data = seg_data.permute(2, 0, 1)  # Adjust the dimensions
