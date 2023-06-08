e05f378e50e69c5205d786b4412663fe28c2a95c
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-08 16:34:35 +0200

    l

--- eval.py
+++ eval.py
@@ -212,6 +212,7 @@ if __name__ == '__main__':
 
         with torch.no_grad():
             volume_in = volume_in # Add a batch dimension
+            volume_in = torch.squeeze(volume_in, dim=0)
             print("volume_in",volume_in.shape)
             seg_pred = segnet  # Assuming the segmentation data is stored in the first channel
             seg_pred = torch.squeeze(seg_pred, dim=0)
