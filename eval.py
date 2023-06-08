9f02de6d4ee960250fe37ced3cd214a435c00e7e
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-08 15:17:57 +0200

    l

--- eval.py
+++ eval.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
             
 
         with torch.no_grad():
-            volume_in = volume_in # Add a batch dimension
+            volume_in = volume_in.unsqueeze(0) # Add a batch dimension
             print("volume_in",volume_in.shape)
             seg_pred = segnet  # Assuming the segmentation data is stored in the first channel
             print("seg_pred shape:", seg_pred.shape)
@@ -220,7 +220,7 @@ if __name__ == '__main__':
             counter = 1  # Initialize the counter
             if surf_hemi == 'lh':
                 seg = (seg_pred==1).cpu().numpy()  # lh
-                #seg = seg[2:-2, :, :]  # Remove padding
+                seg = seg[2:-2, :, :]  # Remove padding
                 print("seg shape:", seg.shape)
                 
                     
