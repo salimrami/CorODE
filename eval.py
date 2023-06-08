2d146182125f2cb7bbe41ed172e9eacef35b0ea1
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-08 16:00:19 +0200

    l

--- eval.py
+++ eval.py
@@ -220,7 +220,7 @@ if __name__ == '__main__':
             counter = 1  # Initialize the counter
             if surf_hemi == 'lh':
                 seg = (seg_pred==1).cpu().numpy()  # lh
-                seg = seg[2:-2, :, :]  # Remove padding
+                #seg = seg[2:-2, :, :]  # Remove padding
                 print("seg shape:", seg.shape)
                 
                     
