169fe7f332768ce48bb45413840dbddb7bd21b1c
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-09 15:01:25 +0200

    l

--- eval.py
+++ eval.py
@@ -282,12 +282,14 @@ if __name__ == '__main__':
         if test_type == 'init':
             mesh_init = trimesh.Trimesh(v_in, f_in)
             mesh_init.export(init_dir+'init_'+data_name+'_'+surf_hemi+'_'+subid+'.obj')
-            file_name = 'init_' + data_name + '_' + surf_hemi + '_' + subid + '.obj'
             # Chemin de destination pour sauvegarder le maillage initial
-            save_path = file_name + init_dir
+            #save_path = '/scratch/saiterrami/results/init_surface.obj'
+            # Sauvegarder le maillage initial en tant qu'objet Wavefront (.obj)
+            #mesh_init.export(save_path)
+            save_path = '/scratch/saiterrami/init/init_surface.obj'
+
             # Sauvegarder le maillage initial en tant qu'objet Wavefront (.obj)
             mesh_init.export(save_path)
-            
 
         # ------- predict cortical surfaces ------- 
         if test_type == 'pred' or test_type == 'eval':
