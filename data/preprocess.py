e48137dfc25f3c4d28a62b16e6d55830a4027cb5
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-15 16:46:14 +0200

    pre

--- data/preprocess.py
+++ data/preprocess.py
@@ -31,7 +31,7 @@ def process_surface(v, f, data_name='fetal'):
         
         
             # Normalize to [-1, 1]
-        #v = (v - [112, 125, 101] )/ 112
+        v = (v - [112, 125, 101] )/ 112
         print("Data has been normalized. Min value:", np.min(v), "Max value:", np.max(v))
 
     
@@ -43,7 +43,7 @@ def process_surface(v, f, data_name='fetal'):
 
 def process_surface_inverse(v, f, data_name='fetal'):
     if data_name == 'fetal':
-        #v = v * 112 + [112, 125.5, 101]
+        v = v * 112 + [112, 125.5, 101]
         v = v[:,[2,1,0]].copy()
         f = f[:,[2,1,0]].copy()
     else:
