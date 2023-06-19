80c00514d656174d3a470eedac08c9dde91ccde2
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-19 15:35:15 +0200

    l

--- data/preprocess.py
+++ data/preprocess.py
@@ -32,7 +32,7 @@ def process_surface(v, f, data_name='fetal'):
         
             # Normalize to [-1, 1]
         v = (v - [104, 104, 78]) / 104
-        #v = np.clip(v, -1, 1) 
+        v = np.clip(v, -1, 1) 
         print("Data has been normalized. Min value:", np.min(v), "Max value:", np.max(v))
 
     
