803df2779d8e3830f07eda281f52531c3aacc90f
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-20 10:59:41 +0200

    l

--- eval.py
+++ eval.py
@@ -79,6 +79,7 @@ def seg2surf(seg,
 
     f_mc = f_mc.copy()
     D1,D2,D3 = sdf_topo.shape
+    print(D1,D2,D3)
     D = max(D1,D2,D3)
     #jai decommenté ca pour la normalisation des surface pial et white
     v_mc = (2*v_mc - [D3, D2, D1]) / D   # rescale to [-1,1]
