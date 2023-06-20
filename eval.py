e0ba544ae583bbd3d629395dad6f6ef9179dcf5d
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-06-20 10:08:14 +0200

    l

--- eval.py
+++ eval.py
@@ -193,7 +193,7 @@ if __name__ == '__main__':
         if data_name == 'fetal':
             brain = nib.load(data_dir+subid+'/'+subid+'_T2w.nii.gz')
             brain_arr = brain.get_fdata()
-            brain_arr = (brain_arr / 700).astype(np.float16)
+            brain_arr = (brain_arr / 1007).astype(np.float16)
             brain_arr = brain_arr[2:-2, :, :]  # Remove padding
         brain_arr = process_volume(brain_arr, data_name)
         volume_in = torch.Tensor(brain_arr).unsqueeze(0).to(device)
