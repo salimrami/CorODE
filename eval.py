b4271e8f3d116606a8fd07f131a6dcfabd8a3dae
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-07-17 13:52:46 +0200

    l

--- eval.py
+++ eval.py
@@ -75,7 +75,7 @@ def seg2surf(seg,
 
     # ------ marching cubes ------
     v_mc, f_mc, _, _ = marching_cubes(-sdf_topo, level=-level, method='lorensen')
-    v_mc = v_mc[:,[2,1,0]].copy()
+    #v_mc = v_mc[:,[2,1,0]].copy()
     #v_mc = v_mc[:,[0,1,2]].copy()
 
     f_mc = f_mc.copy()
@@ -105,7 +105,7 @@ def seg2surf(seg,
     for j in range(n_smooth):    # smooth and inflate the mesh
         v_mc = laplacian_smooth(v_mc, f_mc, 'uniform', lambd=1)
     v_mc = v_mc[0].cpu().numpy()
-    v_mc = v_mc[:,[0,1,2]].copy()
+    #v_mc = v_mc[:,[0,1,2]].copy()
     f_mc = f_mc[0].cpu().numpy()
     
     return v_mc, f_mc
