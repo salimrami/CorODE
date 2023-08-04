01117279fa3eec807935314149b72dde145b7c25
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-08-04 21:55:33 +0200

    l

--- eval.py
+++ eval.py
@@ -73,10 +73,11 @@ def seg2surf(seg,
     
     
 
-
-
     cc_id = 1 + np.argmax(np.array([np.count_nonzero(cc == i)\
                                     for i in range(1, nc+1)]))
+
+    #cc_id = 1 + np.argmax(np.array([np.count_nonzero(cc == i)\
+                                    #for i in range(1, nc+1)]))
     seg = (cc==cc_id).astype(np.float64)
 
     # ------ generate signed distance function ------ 
@@ -199,7 +200,7 @@ if __name__ == '__main__':
         if data_name == 'fetal':
             brain = nib.load(data_dir+subid+'/'+subid+'_T2w.nii.gz')
             brain_arr = brain.get_fdata()
-            brain_arr = (brain_arr / 1773).astype(np.float16)
+            brain_arr = (brain_arr / 20).astype(np.float16)
            # brain_arr = brain_arr[2:-2, :, :]  # Remove padding
         brain_arr = process_volume(brain_arr, data_name)
         volume_in = torch.Tensor(brain_arr).unsqueeze(0).to(device)
