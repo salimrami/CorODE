58775734cc4096906a95e49f8d55d974cd717bf9
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-08-20 00:32:19 +0200

    l

--- eval.py
+++ eval.py
@@ -143,35 +143,14 @@ if __name__ == '__main__':
             brain = nib.load(data_dir+subid+'/'+subid+'_T2w.nii.gz')
             
             brain_arr = brain.get_fdata()
-            #brain_arr = (brain_arr / 1500.).astype(np.float16)
-       #brain_arr = process_volume(brain_arr, data_name)
-        #volume_in = torch.Tensor(brain_arr).unsqueeze(0).to(device)
+            brain_arr = (brain_arr / 40).astype(np.float16)
+        brain_arr = process_volume(brain_arr, data_name)
+        volume_in = torch.Tensor(brain_arr).unsqueeze(0).to(device)
         #calculer min et max adni 
             
             
             
-            
-            min_value = np.min(brain_arr)
-            print("le min : ",min_value)
-            max_value = np.max(brain_arr)
-            print("le max : ",max_value)
-            median =np.mean(brain_arr)
-            print("le median : ",median)
 
-# Define the desired range for voxel intensities
-            desired_min = 0  # Update with your desired minimum intensity value
-            desired_max = 255  # Update with your desired maximum intensity value
-
-# Calculate the scaling factor
-            scaling_factor = (desired_max - desired_min) / (max_value - min_value)
-            
-            
-            
-            brain_arr = (((brain_arr - min_value) * scaling_factor) + desired_min)
-            brain_arr = (brain_arr / 20).astype(np.float16)
-            
-        brain_arr = process_volume(brain_arr, data_name)
-        volume_in = torch.Tensor(brain_arr).unsqueeze(0).to(device)
             
 
         # ------- predict segmentation ------- 
