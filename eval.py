47339d85272a5c7fbc43837ae9429c984b0950db
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-08-20 19:35:53 +0200

    l

--- eval.py
+++ eval.py
@@ -299,8 +299,10 @@ if __name__ == '__main__':
         v_in, f_in = process_surface_inverse(v_in, f_in, data_name)
         mesh_init = trimesh.Trimesh(v_in, f_in)
         mesh_init.export('/scratch/saiterrami/init/init.obj')
-        nib.freesurfer.io.write_geometry(result_dir+data_name+'init''_''.white',
-                                         v_in, f_in)
+        output_file_path = os.path.join(result_dir, data_name + 'init_white')
+        nib.freesurfer.io.write_geometry(output_file_path, v_in, f_in)
+
+        
         gii = nib.gifti.GiftiImage()
         gii.add_gifti_data_array(nib.gifti.GiftiDataArray(v_in, intent='NIFTI_INTENT_POINTSET'))
         gii.add_gifti_data_array(nib.gifti.GiftiDataArray(f_in, intent='NIFTI_INTENT_TRIANGLE'))
