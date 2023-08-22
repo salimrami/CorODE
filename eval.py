ac2cbefa10c8c0aa7d47e2ef738dca5d6ca79623
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-08-22 15:24:18 +0200

    l

--- eval.py
+++ eval.py
@@ -422,15 +422,16 @@ if __name__ == '__main__':
 
         # ------- evaluation -------
         if test_type == 'eval':
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
             v_wm_pred = torch.Tensor(v_wm_pred).unsqueeze(0).to(device)
             f_wm_pred = torch.LongTensor(f_wm_pred).unsqueeze(0).to(device)
             v_gm_pred = torch.Tensor(v_gm_pred).unsqueeze(0).to(device)
             f_gm_pred = torch.LongTensor(f_gm_pred).unsqueeze(0).to(device)
 
-            v_wm_gt = torch.Tensor(v_in)
-            f_wm_gt = torch.LongTensor(f_in)
-            v_gm_gt = torch.Tensor(v_in)
-            f_gm_gt = torch.LongTensor(f_in)
+            v_wm_gt =(v_in)
+            f_wm_gt =(f_in)
+            v_gm_gt =(v_in)
+            f_gm_gt =(f_in)
 
             # compute ASSD and HD
             assd_wm, hd_wm = compute_mesh_distance(v_wm_pred, v_wm_gt, f_wm_pred, f_wm_gt)
